3f71c8b589bf660422b0942435f377f87ce31bee
Eduardo Lopez <88441218+KnaitSakuray@users.noreply.github.com>
2022-10-14 10:25:47 -0300

    Update Conexao.cs

--- CHECK LIST DE PROCESSO/Class/Conexao.cs
+++ CHECK LIST DE PROCESSO/Class/Conexao.cs
@@ -9,7 +9,7 @@ namespace CHECK_LIST_DE_PROCESSO.Class
     class Conexao
     {
         /*
-        private static string _ROTA = "Data Source = 192.168.108.16\\SQLEXPRESS; Initial Catalog = ROTA; Persist Security Info=True;User ID = crud; Password=2B2XWXMV";
+        private static string _ROTA = "";
         public static string ROTA
         {
             get { return _ROTA; }
